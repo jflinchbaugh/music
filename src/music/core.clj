6156e8e3ee8683c15d558c0c3636f4a807ba37d3
John Flinchbaugh <john@hjsoft.com>
2019-07-06 09:42:38 -0400

    bring in parameterized beats

--- src/music/core.clj
+++ src/music/core.clj
@@ -16,12 +16,28 @@
 
 (def live-pats (atom pats))
 
+(defn flatten1
+  "Takes a map and returns a seq of all the key val pairs:
+      (flatten1 {:a 1 :b 2 :c 3}) ;=> (:b 2 :c 3 :a 1)"
+  [m]
+  (reduce (fn [r [arg val]] (cons arg (cons val r))) [] m))
+
 (defn live-sequencer
   ([curr-t sep-t live-patterns] (live-sequencer curr-t sep-t live-patterns 0))
   ([curr-t sep-t live-patterns beat]
    (doseq [[sound pattern] @live-patterns
-           :when (= 1 (nth pattern (mod beat (count pattern))))]
-     (at curr-t (sound)))
+           :let [v (nth pattern (mod beat (count pattern)))
+                 v (cond
+                     (= 1 v)
+                     []
+
+                     (map? v)
+                     (flatten1 v)
+
+                     :else
+                     nil)]
+           :when v]
+     (at curr-t (apply sound v)))
    (let [new-t (+ curr-t sep-t)]
      (apply-by new-t #'live-sequencer [new-t sep-t live-patterns (inc beat)]))))
 
@@ -29,12 +45,32 @@
 
   (live-sequencer (+ 200 (now)) 200 live-pats)
 
-  (swap! live-pats assoc subby [1 1 0 1 0 0 1 1])
-
-  (swap! live-pats assoc snare [1 1 0 0 0 1 0 0])
-
-  (swap! live-pats assoc wop   [1 0 1 0 0 0 1 1])
-
   (stop)
+
+  (do
+    (swap! live-pats assoc subby [0 0 0 0 0 0 0 0])
+    (swap! live-pats assoc snare [0 0 0 0 0 0 0 0])
+    (swap! live-pats assoc wop   [0 0 0 0 0 0 0 0])
+  )
+
+  (swap! live-pats assoc subby [1 1 0 1 0 0 1 1])
+  (swap! live-pats assoc snare [1 1 0 0 0 1 0 0])
+  (swap! live-pats assoc wop   [1 0 0 0 0 0 0 0])
+
+  (swap! live-pats assoc subby [1 0 0 0 1 0 0 0 1 0 0 1 0 1 0 0])
+  (swap! live-pats assoc snare [1 1 0 1 0 1 0 1])
+
+  (swap! live-pats assoc wop   [1 0 0 1 1 0 0 1])
+
+  (swap! live-pats assoc p/sampled-piano  [0 0 0 0 0 0 0])
+  (swap! live-pats dissoc p/sampled-piano)
+
+  (def a {:rate 0.5})
+  (def b {:rate 3})
+  (def c {:rate 10})
+
+  (swap! live-pats assoc subby [1 1 0 b 0 1 a c])
+  (swap! live-pats assoc snare [1 1 c c 1 a b c])
+  (swap! live-pats assoc wop   [c c 1 0 0 0 a c])
 
   )
