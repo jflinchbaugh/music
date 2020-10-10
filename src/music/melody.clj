6ad3da1ebc73609f77b4e8fd85e06cba3ad1fb7f
John Flinchbaugh <john@hjsoft.com>
2020-10-10 12:40:06 -0400

    beat-length, note-duration, smarter repeats

--- src/music/melody.clj
+++ src/music/melody.clj
@@ -2,43 +2,31 @@
   (:require [overtone.live :refer :all]
             [overtone.synth.stringed :refer :all]))
 
-(definst foo [freq 400] (* 0.3 (saw freq)))
-
 (definst ding [freq 400 amp 0.5 sustain 0.5]
   (* amp (env-gen (perc 0.02 sustain 1 -1) :action FREE) (pan2 (sin-osc freq))))
 
 (definst whistle [freq 400 amp 0.5 attack 0.01 sustain 0.5 release 0.01]
-  (* amp (env-gen (lin attack (- sustain attack release) release) :action FREE) (pan2 (sin-osc freq))))
+  (* amp (env-gen (lin attack (- sustain attack release) release) :action FREE) (pan2 (square freq))))
 
 (comment
   (let [chord-name :major
-        bpm 174
+        bpm 140
+        beat-length 1/2
         metro (metronome bpm)
-        progression-1 (take 3 (shuffle (scale :c4 chord-name)))
-        progression-2 (take 3 (shuffle (scale :e4 chord-name)))
+        note-duration (* 1/1000 beat-length (- (metro 1) (metro 0)))
+        progression-1 (take 4 (shuffle (scale :c4 chord-name)))
+        progression-2 (take 4 (shuffle (scale :d4 chord-name)))
         stream (map #(-> [%1 %2])
                  (concat
-                   progression-1
-                   [0]
-                   progression-1
-                   [0]
-                   progression-2
-                   [0]
-                   progression-2
-                   [0]
-                   progression-1
-                   [0]
-                   progression-1
-                   [0]
-                   progression-2
-                   [0]
-                   progression-2
-                   [0])
-                 (range))
+                   (flatten (repeat 4 progression-1))
+                   (flatten (repeat 4 progression-2))
+                   (flatten (repeat 4 progression-1))
+                   (flatten (repeat 4 progression-2))
+                   )
+                 (map #(* % beat-length) (range)))
         ]
-    (prn (map first stream))
+    (prn progression-1 progression-2)
     (doseq [[n b] stream]
-      (at (metro b) (whistle (midi->hz n) 1 0.01 0.3 0.01))))
-
+      (at (metro (inc b)) (whistle (midi->hz n) 0.75 0.02 note-duration 0.02))))
 
   )
