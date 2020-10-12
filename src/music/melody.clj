b807f6f811ce4924a0d83ed4eee33c4015a7817d
John Flinchbaugh <john@hjsoft.com>
2020-10-12 19:29:25 -0400

    more instruments

--- src/music/melody.clj
+++ src/music/melody.clj
@@ -2,20 +2,33 @@
   (:require [overtone.live :refer :all]
             [overtone.synth.stringed :refer :all]))
 
-(definst ding [freq 400 amp 0.5 sustain 0.5]
-  (* amp (env-gen (perc 0.02 sustain 1 -1) :action FREE) (pan2 (sin-osc freq))))
+(definst ding [freq 400 amp 0.5 attack 0.02 sustain 0.5 release 0.01]
+  (* amp (env-gen (perc attack sustain 1 -1) :action FREE) (pan2 (sin-osc freq))))
 
 (definst whistle [freq 400 amp 0.5 attack 0.01 sustain 0.5 release 0.01]
-  (* amp (env-gen (lin attack (- sustain attack release) release) :action FREE) (pan2 (square freq))))
+  (* amp
+    (env-gen (lin attack (- sustain attack release) release) :action FREE)
+    (pan2 (sin-osc freq))))
+
+(definst square-buzz [freq 400 amp 0.5 attack 0.01 sustain 0.5 release 0.01]
+  (* amp
+    (env-gen (lin attack (- sustain attack release) release) :action FREE)
+    (pan2 (square freq))))
+
+(definst saw-buzz [freq 400 amp 0.5 attack 0.01 sustain 0.5 release 0.01]
+  (* amp
+    (env-gen (lin attack (- sustain attack release) release) :action FREE)
+    (pan2 (saw freq))))
 
 (comment
   (let [chord-name :major
+        instrument saw-buzz
         bpm 140
-        beat-length 1/2
+        beat-length 1/4
         metro (metronome bpm)
         note-duration (* 1/1000 beat-length (- (metro 1) (metro 0)))
         progression-1 (take 4 (shuffle (scale :c4 chord-name)))
-        progression-2 (take 4 (shuffle (scale :d4 chord-name)))
+        progression-2 (take 4 (shuffle (scale :c4 chord-name)))
         stream (map #(-> [%1 %2])
                  (concat
                    (flatten (repeat 4 progression-1))
@@ -27,6 +40,6 @@
         ]
     (prn progression-1 progression-2)
     (doseq [[n b] stream]
-      (at (metro (inc b)) (whistle (midi->hz n) 0.75 0.02 note-duration 0.02))))
+      (at (metro (inc b)) (instrument (midi->hz n) 0.75 0.02 note-duration 0.02))))
 
   )
