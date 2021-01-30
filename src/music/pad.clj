92a9d5bbaf46423b48802085053c6b6c3f58a7c4
John Flinchbaugh <john@hjsoft.com>
2021-01-30 01:35:18 -0500

    transforms

--- src/music/pad.clj
+++ src/music/pad.clj
@@ -12,19 +12,25 @@
              :green 88
              :red 104})
 
+(defn coord->note [row col]
+  (+ (* 16 row) col))
+
+(defn note->coord [note]
+  [(int (/ note 16)) (mod note 16)])
+
 (defn light-off [pad]
-  (let [note (if (coll? pad) (+ (* 16 (first pad)) (second pad)) pad)]
+  (let [note (if (coll? pad) (apply coord->note pad) pad)]
     (midi-note-off rcv note)))
 
 (defn light-on
   ([pad color]
-   (let [note (if (coll? pad) (+ (* 16 (first pad)) (second pad)) pad)
+   (let [note (if (coll? pad) (apply coord->note pad) pad)
          vel (get colors color color)]
      (do
        (light-off pad)
        (midi-note-on rcv note vel))))
   ([pad color dur]
-   (let [note (if (coll? pad) (+ (* 16 (first pad)) (second pad)) pad)
+   (let [note (if (coll? pad) (apply coord->note pad) pad)
          vel (get colors color color)]
      (do
        (light-off pad)
@@ -34,10 +40,12 @@
   (let [cc (cycle (rest (keys colors)))]
     (doseq [r (range 8)
             c (range 8)]
-      (light-on [r c] (nth cc (+ r c)) 100)))
+      (light-on [r c] (nth cc (+ r c)))))
 
-  (light-on [1 0] :white 100)
+  (light-on [0 2] :white)
 
   (midi-connected-receivers)
+
+  (apply coord->note (note->coord 10))
 
   .)
