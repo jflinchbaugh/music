883b27c0989a7cd3a0245d897ba2d87adec7f280
John Flinchbaugh <john@hjsoft.com>
2021-05-09 21:00:09 -0400

    player/debug framework for pad

--- src/music/pad.clj
+++ src/music/pad.clj
@@ -40,21 +40,52 @@
   "Light up the whole grid with colors determined
   by calling (color-fn row column)."
   [color-fn]
-    (doseq [r (range 8)
-            c (range 8)]
-      (light-on [r c] (color-fn r c))))
+  (doseq [r (range 8)
+          c (range 8)]
+    (light-on [r c] (color-fn r c))))
 
 (defn random-lights [r c]
   (let [cc (cycle (rest (keys colors)))]
     (-> cc shuffle first)))
 
+(defonce active-players* (atom '()))
+
+(defn start-player [player & args]
+  (swap! active-players* conj (apply player args)))
+
+(defn stop-inst-player [event-handler-ids]
+  "Given a list of event-handler-ids returned by inst-player, remove
+  all event handlers."
+  (doseq [id event-handler-ids]
+    (remove-event-handler id)))
+
+(defn stop-active-players []
+  (doseq [p @active-players*]
+    (stop-inst-player p)
+    (prn p)
+    (swap! active-players* (fn [coll item] (remove #{item} coll)) p)))
+
+(defn pad-handler []
+  (let [id (keyword (gensym "on-handler"))]
+    (on-event [:midi :note-on]
+      (fn [{:keys [note velocity-f]}]
+        (prn (note->coord note)))
+      id)
+    [id]))
+
 (comment
   (light-grid (fn [r c] (first (shuffle (rest (keys colors))))))
+
+  (light-grid (fn [r c] (nth (cycle (rest (keys colors))) (+ (* 2 c) (* 2 r)))))
 
   (light-on [0 2] :white)
 
   (midi-connected-receivers)
 
   (apply coord->note (note->coord 10))
+
+  (start-player pad-handler)
+
+  (stop-active-players)
 
   .)
