efee8de2dcf4b06f799586d7fdb27a5c6b9c27aa
John Flinchbaugh <john@hjsoft.com>
2021-10-17 15:22:13 -0400

    lots of playing from tutorials and wiki

--- src/music/scratch.clj
+++ src/music/scratch.clj
@@ -1,5 +1,5 @@
 (ns music.scratch
-  (:require [overtone.live :refer :all]
+  (:require [overtone.core :refer :all]
             [music.core :refer :all]
             [overtone.inst.drum :as d]
             [overtone.inst.sampled-piano :as sp]
@@ -320,6 +320,138 @@
 
 
   (kick 100)
+
+  ;; degrees
+
+  (def metro (metronome 120))
+
+  (def scale-degrees [:vi :vii :i+ :_ :vii :_ :i+ :vii :vi :_ :vii :_])
+
+  (def pitches (degrees->pitches scale-degrees :dorian :C4))
+
+  (iterate #(prn %) pitches)
+
+  (defn play [time notes sep]
+    (let [note (first notes)]
+      (when note
+        (at time (saw (midi->hz note))))
+      (let [next-time (+ time sep)]
+        (apply-by next-time #'play [next-time (rest notes) sep]))))
+
+  (play (+ 1000 (now)) pitches 200000)
+
+  (metro (metro))
+
+  (metro)
+
+  (stop)
+
+
+  ;; playing notes
+
+  ;; We use a saw-wave that we defined in the oscillators tutorial
+  (definst saw-wave [freq 440 attack 0.01 sustain 0.7 release 0.5 vol 0.4]
+    (* (env-gen (env-lin attack sustain release) 1 1 0 1 FREE)
+      (saw freq)
+      vol))
+
+  ;; We can play notes using frequency in Hz
+  (saw-wave 440)
+  (saw-wave 523.25)
+  (saw-wave 261.62) ; This is C4
+
+  ;; We can also play notes using MIDI note values
+  (saw-wave (midi->hz 69))
+  (saw-wave (midi->hz 72))
+  (saw-wave (midi->hz 60)) ; This is C4
+
+  ;; We can play notes using standard music notes as well
+  (saw-wave (midi->hz (note :A4)))
+  (saw-wave (midi->hz (note :C5)))
+  (saw-wave (midi->hz (note :C4))) ; This is C4! Surprised?
+
+  ;; Define a function for convenience
+  (defn note->hz [music-note]
+    (midi->hz (note music-note)))
+
+  ; Slightly less to type
+  (saw-wave (note->hz :C5))
+
+  ;; Let's make it even easier
+  (defn saw2 [music-note]
+    (saw-wave :freq (midi->hz (note music-note)) :vol 0.2))
+
+  ;; Great!
+  (saw2 :A4)
+  (saw2 :C5)
+  (saw2 :C4)
+
+  ;; Let's play some chords
+
+
+  ;; this is one possible implementation of play-chord
+  (defn play-chord [a-chord]
+    (doseq [note (take 1 a-chord)] (saw2 note)))
+
+  ;; We can play many types of chords.
+  ;; For the complete list, visit https://github.com/overtone/overtone/blob/master/src/overtone/music/pitch.clj and search for "def CHORD"
+  (play-chord (chord :C4 :major))
+
+  ;; We can play a chord progression on the synth
+  ;; using times:
+  (defn chord-progression-time []
+    (let [time (now)]
+      (at time (play-chord (chord :C4 :major)))
+      (at (+ 2000 time) (play-chord (chord :G3 :major)))
+      (at (+ 3000 time) (play-chord (chord :F3 :sus4)))
+      (at (+ 4300 time) (play-chord (chord :F3 :major)))
+      (at (+ 5000 time) (play-chord (chord :G3 :major)))))
+
+  (chord-progression-time)
+
+  ;; or beats:
+  (defonce metro (metronome 120))
+  (metro)
+  (defn chord-progression-beat [m beat-num]
+    (at (m (+ 0 beat-num)) ((var play-chord) (chord :C4 :major)))
+    (at (m (+ 4 beat-num)) ((var play-chord) (chord :G3 :major)))
+    (at (m (+ 8 beat-num)) ((var play-chord) (chord :A3 :minor)))
+    (at (m (+ 14 beat-num)) ((var play-chord) (chord :F3 :major)))  
+  )
+
+  (chord-progression-beat metro (metro))
+
+  ;; We can use recursion to keep playing the chord progression
+  (defn chord-progression-beat [m beat-num]
+    (at (m (+ 0 beat-num)) (#'play-chord (chord :C4 :major)))
+    (at (m (+ 1 beat-num)) (#'play-chord (chord :C4 :major)))
+    (at (m (+ 2 beat-num)) (#'play-chord (chord :C4 :major)))
+    (at (m (+ 3 beat-num)) (#'play-chord (chord :C4 :major)))
+    (at (m (+ 4 beat-num)) (#'play-chord (chord :G3 :major)))
+    (at (m (+ 6 beat-num)) (#'play-chord (chord :G3 :major)))
+    (at (m (+ 8 beat-num)) (#'play-chord (chord :A3 :minor)))
+    (at (m (+ 10 beat-num)) (#'play-chord (chord :A3 :minor)))
+    (at (m (+ 12 beat-num)) (#'play-chord (chord :F3 :major)))
+    (at (m (+ 13 beat-num)) (#'play-chord (chord :F3 :major)))
+    (at (m (+ 14 beat-num)) (#'play-chord (chord :F3 :major)))
+    (at (m (+ 15 beat-num)) (#'play-chord (chord :F3 :major)))
+
+    (at (m (+ 0 beat-num)) (d/kick))
+    #_(at (m (+ 2 beat-num)) (d/kick))
+    (at (m (+ 6 beat-num)) (d/kick))
+    (at (m (+ 10 beat-num)) (d/kick))
+    (at (m (+ 14 beat-num)) (d/kick))
+    #_(at (m (+ 14.5 beat-num)) (d/kick))
+    #_(at (m (+ 15 beat-num)) (d/kick))
+    #_(at (m (+ 15.5 beat-num)) (d/kick))
+
+    #_(apply-by (m (+ 16 beat-num)) (var chord-progression-beat) m (+ 16 beat-num) [])
+  )
+
+  (chord-progression-beat metro (metro))
+
+  (stop)
+
 
   nil)
 
